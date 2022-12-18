86474ec3e6292862ceb1af58a884c982f5626db2
P0PC0RN <2982538245@qq.com>
2022-12-19 00:10:59 +0800

    initial commit

---
+++ src/context/mockData.js/mockFollowers.js
@@ -0,0 +1,22 @@
+export default [
+  {
+"login": "larry-xue",
+"id": 48818060,
+"node_id": "MDQ6VXNlcjQ4ODE4MDYw",
+"avatar_url": "https://avatars.githubusercontent.com/u/48818060?v=4",
+"gravatar_id": "",
+"url": "https://api.github.com/users/larry-xue",
+"html_url": "https://github.com/larry-xue",
+"followers_url": "https://api.github.com/users/larry-xue/followers",
+"following_url": "https://api.github.com/users/larry-xue/following{/other_user}",
+"gists_url": "https://api.github.com/users/larry-xue/gists{/gist_id}",
+"starred_url": "https://api.github.com/users/larry-xue/starred{/owner}{/repo}",
+"subscriptions_url": "https://api.github.com/users/larry-xue/subscriptions",
+"organizations_url": "https://api.github.com/users/larry-xue/orgs",
+"repos_url": "https://api.github.com/users/larry-xue/repos",
+"events_url": "https://api.github.com/users/larry-xue/events{/privacy}",
+"received_events_url": "https://api.github.com/users/larry-xue/received_events",
+"type": "User",
+"site_admin": false
+}
+];
